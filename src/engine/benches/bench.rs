42cfacdfe3b68b93cbd93f6e2dc5493ef250d204
Richard <huachao.huang@gmail.com>
2022-08-11 10:47:51 +0800

    Add a bench tool

--- src/engine/benches/bench.rs
+++ src/engine/benches/bench.rs
@@ -1,47 +1,230 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use photondb_engine::tree::*;
+use std::{
+    sync::{
+        atomic::{AtomicU64, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
 
-const NUM_KEYS: u64 = 10_000_000;
-const NUM_STEPS: u64 = 100;
-const NUM_OPS_PER_STEP: u64 = 100;
-const STEP: usize = (NUM_KEYS / NUM_STEPS) as usize;
+use clap::Parser;
+use photondb_engine::tree;
+use rand::{
+    distributions::{Distribution, Uniform, WeightedIndex},
+    rngs::ThreadRng,
+};
 
-fn get(table: &Table, k: u64) {
-    let buf = k.to_be_bytes();
-    let key = buf.as_slice();
-    table.get(key, 0, |_| {}).unwrap();
+#[derive(Parser, Debug)]
+struct Config {
+    #[clap(long, default_value = "1000000")]
+    pub num_kvs: u64,
+    #[clap(long, default_value = "8")]
+    pub key_len: usize,
+    #[clap(long, default_value = "100")]
+    pub value_len: usize,
+    #[clap(long, default_value = "1000000")]
+    pub num_ops: u64,
+    #[clap(long, default_value = "1")]
+    pub get_weight: usize,
+    #[clap(long, default_value = "1")]
+    pub put_weight: usize,
+    #[clap(long, default_value = "1")]
+    pub num_threads: usize,
 }
 
-fn put(table: &Table, k: u64) {
-    let buf = k.to_be_bytes();
-    let key = buf.as_slice();
-    table.put(key, 0, key).unwrap();
+struct Bench {
+    inner: Arc<Inner>,
 }
 
-fn bench_function<F>(table: &Table, func: F)
-where
-    F: Fn(&Table, u64),
-{
-    for k in (0..NUM_KEYS).step_by(STEP) {
-        for i in 0..NUM_OPS_PER_STEP {
-            func(table, k + i);
+impl Bench {
+    fn new(cfg: Config) -> Self {
+        Self {
+            inner: Arc::new(Inner::new(cfg)),
+        }
+    }
+
+    fn run(&self) {
+        self.inner.setup();
+
+        let start = Instant::now();
+        let mut threads = Vec::new();
+        for _ in 0..self.inner.cfg.num_threads {
+            let inner = self.inner.clone();
+            threads.push(std::thread::spawn(move || {
+                inner.bench();
+            }));
+        }
+        for thread in threads {
+            thread.join().unwrap();
+        }
+        let elapsed = start.elapsed().as_secs_f64();
+
+        let stats = self.inner.stats.snapshot();
+        println!("{:#?}", stats);
+        println!("{:#?}", self.inner.cfg);
+        println!("{} ops/sec", stats.num_ops as f64 / elapsed);
+    }
+}
+
+struct Inner {
+    cfg: Config,
+    table: Table,
+    stats: AtomicStats,
+}
+
+impl Inner {
+    fn new(cfg: Config) -> Self {
+        Self {
+            cfg,
+            table: Table::open(),
+            stats: AtomicStats::default(),
+        }
+    }
+
+    fn setup(&self) {
+        let mut kbuf = vec![0; self.cfg.key_len];
+        let mut vbuf = vec![0; self.cfg.value_len];
+        let mut workload = Workload::new(&self.cfg);
+        for k in 0..self.cfg.num_kvs {
+            workload.fill_with_num(k, &mut kbuf);
+            workload.fill_with_num(k, &mut vbuf);
+            self.table.put(&kbuf, &vbuf);
+        }
+    }
+
+    fn bench(&self) {
+        let mut kbuf = vec![0; self.cfg.key_len];
+        let mut vbuf = vec![0; self.cfg.value_len];
+        let mut workload = Workload::new(&self.cfg);
+        while self.stats.num_ops.get() < self.cfg.num_ops {
+            let k = workload.rand_num();
+            workload.fill_with_num(k, &mut kbuf);
+            match workload.rand_op() {
+                Op::Get => {
+                    self.table.get(&kbuf);
+                    self.stats.num_gets.inc();
+                }
+                Op::Put => {
+                    workload.fill_with_num(k, &mut vbuf);
+                    self.table.put(&kbuf, &vbuf);
+                    self.stats.num_puts.inc();
+                }
+            }
+            self.stats.num_ops.inc();
         }
     }
 }
 
-fn bench(c: &mut Criterion) {
-    let opts = Options::default();
-    let table = Table::open(opts).unwrap();
-    for k in 0..NUM_KEYS {
-        put(&table, k);
-    }
-    println!("{:?}", table.stats());
-
-    c.bench_function("get", |b| b.iter(|| bench_function(&table, get)));
-    println!("Get {:?}", table.stats());
-    c.bench_function("put", |b| b.iter(|| bench_function(&table, put)));
-    println!("Put {:?}", table.stats());
+struct Table {
+    table: tree::Table,
+    sequence: Counter,
 }
 
-criterion_main!(benches);
-criterion_group!(benches, bench);
+impl Table {
+    fn open() -> Self {
+        let table = tree::Table::open(tree::Options::default()).unwrap();
+        Self {
+            table,
+            sequence: Counter::new(0),
+        }
+    }
+
+    fn get(&self, k: &[u8]) {
+        self.table.get(k, self.sequence.inc(), |_| {}).unwrap();
+    }
+
+    fn put(&self, k: &[u8], v: &[u8]) {
+        self.table.put(k, self.sequence.inc(), v).unwrap();
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Op {
+    Get,
+    Put,
+}
+
+struct Workload {
+    rng: ThreadRng,
+    kv_dist: Uniform<u64>,
+    op_dist: WeightedIndex<usize>,
+    op_choices: [Op; 2],
+}
+
+impl Workload {
+    fn new(cfg: &Config) -> Self {
+        let rng = rand::thread_rng();
+        let kv_dist = Uniform::from(0..cfg.num_kvs);
+        let op_dist = WeightedIndex::new([cfg.get_weight, cfg.put_weight]).unwrap();
+        let op_choices = [Op::Get, Op::Put];
+        Self {
+            rng,
+            kv_dist,
+            op_dist,
+            op_choices,
+        }
+    }
+
+    fn rand_op(&mut self) -> Op {
+        self.op_choices[self.op_dist.sample(&mut self.rng)]
+    }
+
+    fn rand_num(&mut self) -> u64 {
+        self.kv_dist.sample(&mut self.rng)
+    }
+
+    fn fill_with_num(&mut self, k: u64, buf: &mut [u8]) {
+        buf[0..8].copy_from_slice(&k.to_be_bytes());
+    }
+}
+
+#[derive(Debug)]
+struct Stats {
+    num_ops: u64,
+    num_gets: u64,
+    num_puts: u64,
+}
+
+#[derive(Default)]
+struct AtomicStats {
+    num_ops: Counter,
+    num_gets: Counter,
+    num_puts: Counter,
+}
+
+impl AtomicStats {
+    fn snapshot(&self) -> Stats {
+        Stats {
+            num_ops: self.num_ops.get(),
+            num_gets: self.num_gets.get(),
+            num_puts: self.num_puts.get(),
+        }
+    }
+}
+
+struct Counter(AtomicU64);
+
+impl Default for Counter {
+    fn default() -> Self {
+        Self::new(0)
+    }
+}
+
+impl Counter {
+    const fn new(value: u64) -> Self {
+        Self(AtomicU64::new(value))
+    }
+
+    fn get(&self) -> u64 {
+        self.0.load(Ordering::Relaxed)
+    }
+
+    fn inc(&self) -> u64 {
+        self.0.fetch_add(1, Ordering::Relaxed)
+    }
+}
+
+fn main() {
+    let cfg = Config::parse();
+    let bench = Bench::new(cfg);
+    bench.run();
+}
---
+++ src/engine/benches/simple.rs
@@ -0,0 +1,47 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use photondb_engine::tree::*;
+
+const NUM_KEYS: u64 = 10_000_000;
+const NUM_STEPS: u64 = 100;
+const NUM_OPS_PER_STEP: u64 = 100;
+const STEP: usize = (NUM_KEYS / NUM_STEPS) as usize;
+
+fn get(table: &Table, k: u64) {
+    let buf = k.to_be_bytes();
+    let key = buf.as_slice();
+    table.get(key, 0, |_| {}).unwrap();
+}
+
+fn put(table: &Table, k: u64) {
+    let buf = k.to_be_bytes();
+    let key = buf.as_slice();
+    table.put(key, 0, key).unwrap();
+}
+
+fn bench_function<F>(table: &Table, func: F)
+where
+    F: Fn(&Table, u64),
+{
+    for k in (0..NUM_KEYS).step_by(STEP) {
+        for i in 0..NUM_OPS_PER_STEP {
+            func(table, k + i);
+        }
+    }
+}
+
+fn bench(c: &mut Criterion) {
+    let opts = Options::default();
+    let table = Table::open(opts).unwrap();
+    for k in 0..NUM_KEYS {
+        put(&table, k);
+    }
+    println!("{:?}", table.stats());
+
+    c.bench_function("get", |b| b.iter(|| bench_function(&table, get)));
+    println!("Get {:?}", table.stats());
+    c.bench_function("put", |b| b.iter(|| bench_function(&table, put)));
+    println!("Put {:?}", table.stats());
+}
+
+criterion_main!(benches);
+criterion_group!(benches, bench);
