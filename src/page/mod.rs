7c491a88ba9f7440eead0c561abe1ff27f1726c8
w41ter <w41ter.l@gmail.com>
2022-10-17 17:42:26 +0800

    page_store: define write_buffer module API

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -1,6 +1,6 @@
 mod base;
 use base::PageBuilder;
-pub(crate) use base::{PageBuf, PageEpoch, PageKind, PageRef, PageTier};
+pub(crate) use base::{PageBuf, PageEpoch, PageKind, PagePtr, PageRef, PageTier};
 
 mod iter;
 pub(crate) use iter::{ForwardIter, ItemIter, MergingIter, SeekableIter};
