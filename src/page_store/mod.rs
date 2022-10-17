aa737eae4de9a72ecd287aedf41e591a0634185e
Richard <huachao.huang@gmail.com>
2022-10-17 12:50:20 +0800

    Split mutable and immutable txn state

--- src/page_store/mod.rs
+++ src/page_store/mod.rs
@@ -1,13 +1,15 @@
+use std::path::Path;
+
+use crate::env::Env;
+
 mod error;
 pub(crate) use error::{Error, Result};
 
 mod page_txn;
-pub(crate) use page_txn::PageTxn;
+pub(crate) use page_txn::{Guard, PageTxn};
 
 mod page_table;
 use page_table::PageTable;
-
-use crate::env::Env;
 
 pub(crate) struct PageStore<E> {
     env: E,
@@ -15,11 +17,11 @@ pub(crate) struct PageStore<E> {
 }
 
 impl<E: Env> PageStore<E> {
-    pub(crate) async fn open(env: E) -> Result<Self> {
+    pub(crate) async fn open<P: AsRef<Path>>(env: E, path: P) -> Result<Self> {
         todo!()
     }
 
-    pub(crate) fn begin(&self) -> PageTxn {
+    pub(crate) fn guard(&self) -> Guard {
         todo!()
     }
 }
--- src/page_store/page_txn.rs
+++ src/page_store/page_txn.rs
@@ -1,39 +1,49 @@
 use super::Result;
 use crate::page::{PageBuf, PageRef};
 
-pub(crate) struct PageTxn;
+pub(crate) struct Guard;
 
-impl PageTxn {
-    pub(crate) fn alloc_id(&self) -> u64 {
-        todo!()
-    }
-
-    pub(crate) fn dealloc_id(&self, id: u64) {
-        todo!()
-    }
-
-    pub(crate) fn alloc_page(&self, size: usize) -> Result<(u64, PageBuf<'_>)> {
-        todo!()
-    }
-
-    pub(crate) fn dealloc_page(&self, addr: u64) {
-        todo!()
-    }
-
-    pub(crate) async fn read_page(&self, addr: u64) -> Result<PageRef<'_>> {
-        todo!()
+impl Guard {
+    pub(crate) fn begin(&self) -> PageTxn<'_> {
+        PageTxn { guard: self }
     }
 
     pub(crate) fn page_addr(&self, id: u64) -> u64 {
         todo!()
     }
 
-    pub(crate) fn update_page(&self, id: u64, old: u64, new: u64) -> Result<()> {
+    pub(crate) async fn read_page(&self, addr: u64) -> Result<PageRef<'_>> {
+        todo!()
+    }
+}
+
+pub(crate) struct PageTxn<'a> {
+    guard: &'a Guard,
+}
+
+impl<'a> PageTxn<'a> {
+    pub(crate) fn alloc_id(&mut self) -> u64 {
+        todo!()
+    }
+
+    pub(crate) fn dealloc_id(&mut self, id: u64) {
+        todo!()
+    }
+
+    pub(crate) fn alloc_page(&mut self, size: usize) -> Result<(u64, PageBuf<'a>)> {
+        todo!()
+    }
+
+    pub(crate) fn dealloc_page(&mut self, addr: u64) {
+        todo!()
+    }
+
+    pub(crate) fn update_page(&mut self, id: u64, old: u64, new: u64) -> Result<()> {
         // TODO: ensures that old < new so that we can recover the page table in order.
         todo!()
     }
 
-    pub(crate) fn replace_page(&self, id: u64, old: u64, new: u64) -> Result<()> {
+    pub(crate) fn replace_page(&mut self, id: u64, old: u64, new: u64) -> Result<()> {
         // TODO: ensures that old < new so that we can recover the page table in order.
         todo!()
     }
@@ -41,7 +51,7 @@ impl PageTxn {
     pub(crate) fn commit(self) {}
 }
 
-impl Drop for PageTxn {
+impl<'a> Drop for PageTxn<'a> {
     fn drop(&mut self) {
         // abort the transaction
         todo!()
