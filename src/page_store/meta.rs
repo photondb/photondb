1b1a296528af4234abf3e1e111a93acc392058e5
walter <patricknicholas@foxmail.com>
2022-10-17 22:25:24 +0800

    page_store: add defination of VersionEdit (#18)
    * page_store: add defination of VersionEdit
    
    * Update src/page_store/meta.rs
    
    Co-authored-by: Richard <huachao.huang@gmail.com>

---
+++ src/page_store/meta.rs
@@ -0,0 +1,27 @@
+use prost::{alloc::vec::Vec, Message};
+
+#[derive(Clone, PartialEq, Message)]
+pub(crate) struct VersionEdit {
+    #[prost(uint32, repeated, tag = "1")]
+    pub new_files: Vec<u32>,
+    #[prost(uint32, repeated, tag = "2")]
+    pub deleted_files: Vec<u32>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn version_edit_decode_and_encode() {
+        let edit = VersionEdit {
+            deleted_file_set: vec![1, 2, 3],
+            new_files: vec![4, 5, 6],
+        };
+
+        let payload = edit.encode_to_vec();
+        let edit = VersionEdit::decode(payload.as_slice()).unwrap();
+        assert_eq!(edit.deleted_file_set, vec![1, 2, 3]);
+        assert_eq!(edit.new_files, vec![4, 5, 6]);
+    }
+}
