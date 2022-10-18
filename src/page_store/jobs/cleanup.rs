a9ebb28b250075d1ef686264a11de0bdd2db4d1b
w41ter <w41ter.l@gmail.com>
2022-10-18 15:07:53 +0800

    page_store: define Cleanup job releated api

---
+++ src/page_store/jobs/cleanup.rs
@@ -0,0 +1,24 @@
+use crate::page_store::Version;
+
+pub(crate) struct CleanupCtx {
+    // TODO: cancel task
+}
+
+impl CleanupCtx {
+    pub async fn run(self, mut version: Version) {
+        loop {
+            let deleted_files = version.deleted_files();
+
+            let mut next_version = version.wait_next_version().await;
+            std::mem::swap(&mut next_version, &mut version);
+            wait_version_released(next_version).await;
+
+            // Now it is safety to cleanup the version.
+            todo!()
+        }
+    }
+}
+
+async fn wait_version_released(version: Version) {
+    todo!()
+}
