5789de2724715c934d8e25b58e9839fc8d40aac0
zojw <robi@hey.com>
2022-10-24 20:08:49 +0800

    page_store: align to logical_block_size instead of 4k

--- src/page_store/page_file/types.rs
+++ src/page_store/page_file/types.rs
@@ -91,14 +91,22 @@ pub(crate) struct FileMeta {
 
     data_offsets: BTreeMap<u64, u64>, // TODO: reduce this size.
     meta_indexes: Vec<u64>,           // [0] -> page_table, [1] ->  delete page, [2], meta_bloc_end
+
+    block_size: usize,
 }
 
 impl FileMeta {
-    pub(crate) fn new(file_id: u32, indexes: Vec<u64>, offsets: BTreeMap<u64, u64>) -> Self {
+    pub(crate) fn new(
+        file_id: u32,
+        indexes: Vec<u64>,
+        offsets: BTreeMap<u64, u64>,
+        block_size: usize,
+    ) -> Self {
         Self {
             file_id,
             meta_indexes: indexes,
             data_offsets: offsets,
+            block_size,
         }
     }
 
