926dfde1b24250efba321576912cc82be9183b64
Richard <huachao.huang@gmail.com>
2022-10-31 13:24:20 +0800

    Add SortedPageKey and SortedPageValue traits

--- src/page/sorted_page.rs
+++ src/page/sorted_page.rs
@@ -5,6 +5,7 @@ use super::{
     SeekableIterator,
 };
 
+/// Builds a sorted page from an iterator.
 pub(crate) struct SortedPageBuilder<I> {
     base: PageBuilder,
     iter: Option<I>,
@@ -15,9 +16,11 @@ pub(crate) struct SortedPageBuilder<I> {
 impl<'a, I, K, V> SortedPageBuilder<I>
 where
     I: RewindableIterator<Item = (K, V)>,
-    K: EncodeTo,
-    V: EncodeTo,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
+    /// Creates a [`SortedPageBuilder`] that will build a page with the given
+    /// metadata.
     pub(crate) fn new(tier: PageTier, kind: PageKind) -> Self {
         Self {
             base: PageBuilder::new(tier, kind),
@@ -27,10 +30,8 @@ where
         }
     }
 
-    pub(crate) fn size(&self) -> usize {
-        self.base.size(self.content_size)
-    }
-
+    /// Creates a [`SortedPageBuilder`] that will build a page from the given
+    /// iterator.
     pub(crate) fn with_iter(mut self, mut iter: I) -> Self {
         for (k, v) in &mut iter {
             self.num_items += 1;
@@ -41,6 +42,12 @@ where
         self
     }
 
+    /// Returns the size of the page that will be built.
+    pub(crate) fn size(&self) -> usize {
+        self.base.size(self.content_size)
+    }
+
+    /// Builds the page with the given information.
     pub(crate) fn build(mut self, page: &'a mut PageBuf<'_>) {
         self.base.build(page);
         let content = page.content_mut();
@@ -65,8 +72,8 @@ struct SortedPageBuf<K, V> {
 
 impl<K, V> SortedPageBuf<K, V>
 where
-    K: EncodeTo,
-    V: EncodeTo,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
     unsafe fn new(content: &mut [u8], num_items: usize) -> Self {
         let offsets_size = num_items * mem::size_of::<u32>();
@@ -95,8 +102,8 @@ pub(crate) struct SortedPageRef<'a, K, V> {
 
 impl<'a, K, V> SortedPageRef<'a, K, V>
 where
-    K: DecodeFrom + Ord,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
     pub(crate) fn new(page: PageRef<'a>) -> Self {
         let content = page.content();
@@ -182,8 +189,8 @@ impl<'a, K, V> Deref for SortedPageRef<'a, K, V> {
 
 impl<'a, K, V, T> From<T> for SortedPageRef<'a, K, V>
 where
-    K: DecodeFrom + Ord,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
     T: Into<PageRef<'a>>,
 {
     fn from(page: T) -> Self {
@@ -204,8 +211,8 @@ impl<'a, K, V> SortedPageIter<'a, K, V> {
 
 impl<'a, K, V, T> From<T> for SortedPageIter<'a, K, V>
 where
-    K: DecodeFrom,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
     T: Into<SortedPageRef<'a, K, V>>,
 {
     fn from(page: T) -> Self {
@@ -215,8 +222,8 @@ where
 
 impl<'a, K, V> Iterator for SortedPageIter<'a, K, V>
 where
-    K: DecodeFrom + Ord,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
     type Item = (K, V);
 
@@ -232,8 +239,8 @@ where
 
 impl<'a, K, V> SeekableIterator<K> for SortedPageIter<'a, K, V>
 where
-    K: DecodeFrom + Ord,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
     fn seek(&mut self, target: &K) {
         self.next = match self.page.rank(target) {
@@ -245,31 +252,23 @@ where
 
 impl<'a, K, V> RewindableIterator for SortedPageIter<'a, K, V>
 where
-    K: DecodeFrom + Ord,
-    V: DecodeFrom,
+    K: SortedPageKey,
+    V: SortedPageValue,
 {
     fn rewind(&mut self) {
         self.next = 0;
     }
 }
 
-#[cfg(test)]
-impl EncodeTo for u64 {
-    fn encode_size(&self) -> usize {
-        mem::size_of::<u64>()
-    }
+pub(crate) trait SortedPageKey: EncodeTo + DecodeFrom + Clone + Ord {
+    fn as_raw_key(&self) -> &[u8];
 
-    unsafe fn encode_to(&self, enc: &mut Encoder) {
-        enc.put_u64(*self)
-    }
+    fn to_split_key(self) -> Self;
 }
 
-#[cfg(test)]
-impl DecodeFrom for u64 {
-    unsafe fn decode_from(dec: &mut Decoder) -> Self {
-        dec.get_u64()
-    }
-}
+pub(crate) trait SortedPageValue: EncodeTo + DecodeFrom {}
+
+impl<T> SortedPageValue for T where T: EncodeTo + DecodeFrom {}
 
 impl EncodeTo for &[u8] {
     fn encode_size(&self) -> usize {
@@ -289,6 +288,16 @@ impl DecodeFrom for &[u8] {
     }
 }
 
+impl SortedPageKey for &[u8] {
+    fn as_raw_key(&self) -> &[u8] {
+        self
+    }
+
+    fn to_split_key(self) -> Self {
+        self
+    }
+}
+
 impl EncodeTo for Key<'_> {
     fn encode_size(&self) -> usize {
         self.raw.encode_size() + mem::size_of::<u64>()
@@ -305,6 +314,17 @@ impl DecodeFrom for Key<'_> {
         let raw = DecodeFrom::decode_from(dec);
         let lsn = dec.get_u64();
         Self::new(raw, lsn)
+    }
+}
+
+impl<'a> SortedPageKey for Key<'a> {
+    fn as_raw_key(&self) -> &'a [u8] {
+        self.raw
+    }
+
+    fn to_split_key(mut self) -> Self {
+        self.lsn = u64::MAX;
+        self
     }
 }
 
@@ -378,29 +398,33 @@ mod tests {
 
     #[test]
     fn sorted_page() {
-        let data = [(1, 1), (3, 3), (5, 5)];
-        let iter = SliceIter::new(&data);
+        let data = [[1], [3], [5]];
+        let input = data
+            .iter()
+            .map(|v| (v.as_slice(), v.as_slice()))
+            .collect::<Vec<_>>();
+        let iter = SliceIter::new(&input);
         let builder = SortedPageBuilder::new(PageTier::Leaf, PageKind::Data).with_iter(iter);
         let mut buf = alloc_page(builder.size());
         let mut page = PageBuf::new(buf.as_mut());
         builder.build(&mut page);
 
-        let page: SortedPageRef<u64, u64> = SortedPageRef::new(page.into());
+        let page: SortedPageRef<&[u8], &[u8]> = SortedPageRef::new(page.into());
         assert_eq!(page.len(), data.len());
-        assert_eq!(page.get(0), Some((1, 1)));
-        assert_eq!(page.get(1), Some((3, 3)));
-        assert_eq!(page.get(2), Some((5, 5)));
+        assert_eq!(page.get(0), Some(input[0]));
+        assert_eq!(page.get(1), Some(input[1]));
+        assert_eq!(page.get(2), Some(input[2]));
         assert_eq!(page.get(3), None);
-        assert_eq!(page.rank(&0), Err(0));
-        assert_eq!(page.rank(&1), Ok(0));
-        assert_eq!(page.rank(&2), Err(1));
-        assert_eq!(page.rank(&3), Ok(1));
-        assert_eq!(page.rank(&4), Err(2));
-        assert_eq!(page.rank(&5), Ok(2));
+        assert_eq!(page.rank(&[0].as_slice()), Err(0));
+        assert_eq!(page.rank(&[1].as_slice()), Ok(0));
+        assert_eq!(page.rank(&[2].as_slice()), Err(1));
+        assert_eq!(page.rank(&[3].as_slice()), Ok(1));
+        assert_eq!(page.rank(&[4].as_slice()), Err(2));
+        assert_eq!(page.rank(&[5].as_slice()), Ok(2));
 
         let mut iter = SortedPageIter::new(page);
         for _ in 0..2 {
-            for (a, b) in (&mut iter).zip(data) {
+            for (a, b) in (&mut iter).zip(input.clone()) {
                 assert_eq!(a, b);
             }
             iter.rewind();
