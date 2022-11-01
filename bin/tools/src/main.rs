9770420a8d8585e7bfa77786286e9a8d867f6820
w41ter <w41ter.l@gmail.com>
2022-11-01 17:35:37 +0800

    Add stress command

--- bin/tools/src/main.rs
+++ bin/tools/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("It works");
+//! A set of tools for photondb.
+
+mod stress;
+
+use clap::{Parser, Subcommand};
+pub(crate) use photondb::Result;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Stress(stress::Args),
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    match args.cmd {
+        Commands::Stress(args) => stress::run(args)?,
+    }
+    Ok(())
 }
