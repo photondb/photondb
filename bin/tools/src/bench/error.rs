9e55e0a195ce5bcf9bcb0fd3af3f9d9e0198cbb8
zojw <93670789+zojw@users.noreply.github.com>
2022-11-08 11:45:59 +0800

    tools: add fillrandom/fillseq/readrandom/updaterandom bench (#184)

--- bin/tools/src/bench/error.rs
+++ bin/tools/src/bench/error.rs
@@ -1,13 +1,6 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub(crate) enum Error {
-    #[error("Again")]
-    Again,
-    #[error("Corrupted")]
-    Corrupted,
-    #[error("Invalid argument")]
-    InvalidArgument,
-}
+pub(crate) enum Error {}
 
 pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
