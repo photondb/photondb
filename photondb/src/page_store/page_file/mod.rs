bac72a652aca7131ea02e10ec8ae34cc6d7c370a
zojw <robi@hey.com>
2022-11-22 21:46:32 +0800

    integrate with read path

--- photondb/src/page_store/page_file/mod.rs
+++ photondb/src/page_store/page_file/mod.rs
@@ -38,11 +38,12 @@ pub(crate) mod facade {
     use super::{
         constant::{DEFAULT_BLOCK_SIZE, MAX_OPEN_READER_FD_NUM},
         file_reader::{self, MetaReader, ReaderCache},
+        types::PageHandle,
         *,
     };
     use crate::{
         env::{Env, PositionalReader, SequentialWriter},
-        page_store::Result,
+        page_store::{CacheEntry, ClockCache, Result},
     };
 
     pub(crate) const PAGE_FILE_PREFIX: &str = "dat";
@@ -58,6 +59,7 @@ pub(crate) mod facade {
         use_direct: bool,
 
         reader_cache: file_reader::ReaderCache<E>,
+        page_cache: Arc<ClockCache<Vec<u8>>>,
     }
 
     impl<E: Env> PageFiles<E> {
@@ -67,12 +69,14 @@ pub(crate) mod facade {
             let base = base.into();
             let base_dir = env.open_dir(&base).await.expect("open base dir fail");
             let reader_cache = ReaderCache::new(MAX_OPEN_READER_FD_NUM);
+            let page_cache = Arc::new(ClockCache::new(2 << 30, 16, -1, false));
             Self {
                 env,
                 base,
                 base_dir,
                 use_direct,
                 reader_cache,
+                page_cache,
             }
         }
 
@@ -112,6 +116,33 @@ pub(crate) mod facade {
                 use_direct,
                 DEFAULT_BLOCK_SIZE,
             ))
+        }
+
+        pub(crate) async fn read_page(
+            &self,
+            file_id: u32,
+            addr: u64,
+            handle: PageHandle,
+            file_info: &FileInfo,
+        ) -> Result<CacheEntry<Vec<u8>, ClockCache<Vec<u8>>>> {
+            use crate::page_store::Cache;
+
+            if let Some(cache_entry) = self.page_cache.lookup(addr) {
+                return Ok(cache_entry);
+            }
+
+            let reader = self
+                .open_page_reader(file_id, file_info.meta().block_size())
+                .await?;
+            let mut buf = vec![0u8; handle.size as usize];
+            reader.read_exact_at(&mut buf, handle.offset as u64).await?;
+
+            let cache_entry = self
+                .page_cache
+                .insert(addr, Some(buf))
+                .expect("insert cache fail");
+
+            Ok(cache_entry.unwrap())
         }
 
         /// Open page_reader for a page_file.
--- photondb/src/page_store/page_txn.rs
+++ photondb/src/page_store/page_txn.rs
@@ -6,12 +6,14 @@ use std::{
 use super::{
     version::Version,
     write_buffer::{RecordHeader, ReleaseState},
-    Error, PageFiles, PageTable, Result, WriteBuffer, NAN_ID,
+    CacheEntry, ClockCache, Error, PageFiles, PageTable, Result, WriteBuffer, NAN_ID,
 };
 use crate::{
     env::Env,
     page::{PageBuf, PageRef},
 };
+
+type CacheEntryGuard = CacheEntry<Vec<u8>, ClockCache<Vec<u8>>>;
 
 pub(crate) struct Guard<E: Env>
 where
@@ -20,7 +22,7 @@ where
     version: Arc<Version>,
     page_table: PageTable,
     page_files: Arc<PageFiles<E>>,
-    owned_pages: Mutex<Vec<Vec<u8>>>,
+    cache_guards: Mutex<Vec<CacheEntryGuard>>,
 }
 
 impl<E: Env> Guard<E> {
@@ -33,7 +35,7 @@ impl<E: Env> Guard<E> {
             version,
             page_table,
             page_files,
-            owned_pages: Mutex::default(),
+            cache_guards: Mutex::default(),
         }
     }
 
@@ -73,51 +75,36 @@ impl<E: Env> Guard<E> {
             panic!("File {file_id} is not exists");
         };
         assert_eq!(file_info.get_file_id(), file_id);
-        if let Some(map_file_id) = file_info.get_map_file_id() {
+
+        let page_handle = if let Some(map_file_id) = file_info.get_map_file_id() {
             // This is partial page file, read page from the corresponding map file.
             let Some(file_info) = self.version.map_files().get(&map_file_id) else {
                 panic!("Map file {file_id} is not exists");
             };
             assert_eq!(file_info.file_id(), map_file_id);
+
             let Some(handle) = file_info.get_page_handle(addr) else {
                 panic!("The addr {addr} is not belongs to the target map file {file_id}");
             };
 
-            // TODO: cache page file reader for speed up.
-            let reader = self
-                .page_files
-                .open_page_reader(file_id, file_info.meta().block_size())
-                .await?;
-            let mut buf = vec![0u8; handle.size as usize];
-            reader.read_exact_at(&mut buf, handle.offset as u64).await?;
-
-            let mut owned_pages = self.owned_pages.lock().expect("Poisoned");
-            owned_pages.push(buf);
-            let page = owned_pages.last().expect("Verified");
-            let page = page.as_slice();
-
-            return Ok(PageRef::new(unsafe {
-                // Safety: the lifetime is guarranted by `guard`.
-                std::slice::from_raw_parts(page.as_ptr(), page.len())
-            }));
-        }
-
-        let Some(handle) = file_info.get_page_handle(addr) else {
-            panic!("The addr {addr} is not belongs to the target page file {file_id}");
+            handle
+        } else {
+            let Some(handle) = file_info.get_page_handle(addr) else {
+                panic!("The addr {addr} is not belongs to the target page file {file_id}");
+            };
+            handle
         };
 
-        // TODO: cache page file reader for speed up.
-        let reader = self
+        let entry = self
             .page_files
-            .open_page_reader(file_id, file_info.meta().block_size())
+            .read_page(file_id, addr, page_handle, file_info)
             .await?;
-        let mut buf = vec![0u8; handle.size as usize];
-        reader.read_exact_at(&mut buf, handle.offset as u64).await?;
 
-        let mut owned_pages = self.owned_pages.lock().expect("Poisoned");
-        owned_pages.push(buf);
-        let page = owned_pages.last().expect("Verified");
-        let page = page.as_slice();
+        let mut owned_pages = self.cache_guards.lock().expect("Poisoned");
+        owned_pages.push(entry);
+
+        let last_guard = owned_pages.last().unwrap();
+        let page = last_guard.value();
 
         Ok(PageRef::new(unsafe {
             // Safety: the lifetime is guarranted by `guard`.
